0ec381311c108d7c44438c4dfc9f81ab99f94d03
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-02-10 13:58:32 +0100

    dependencies update

--- src/main/java/ch/ethz/idsc/edelweis/LineChart.java
+++ src/main/java/ch/ethz/idsc/edelweis/LineChart.java
@@ -3,39 +3,32 @@ package ch.ethz.idsc.edelweis;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.xy.XYDataset;
 
-import ch.ethz.idsc.subare.plot.ListPlotBuilder;
-import ch.ethz.idsc.subare.plot.XYDatasets;
+import ch.ethz.idsc.subare.util.plot.ListPlot;
+import ch.ethz.idsc.subare.util.plot.VisualSet;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
-import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Sort;
 import ch.ethz.idsc.tensor.alg.Subdivide;
-import ch.ethz.idsc.tensor.alg.Transpose;
 import ch.ethz.idsc.tensor.red.Max;
 import ch.ethz.idsc.tensor.sca.Log;
 
 public enum LineChart {
   ;
   public static void of(Collection<BulkParser> bulkParsers, File directory) {
-    Map<String, Tensor> map = new LinkedHashMap<>();
+    VisualSet visualSet = new VisualSet();
     for (BulkParser bulkParser : bulkParsers) {
       Tensor lineCounts = bulkParser.allLineCounts();
       if (1 < lineCounts.length()) {
         Tensor domain = Subdivide.of(0, 1, lineCounts.length() - 1);
-        Tensor tensor = Transpose.of(Tensors.of(domain, Log.of(Sort.of(lineCounts.map(Max.function(RealScalar.ONE))))));
-        map.put(bulkParser.name(), tensor);
+        Tensor values = Log.of(Sort.of(lineCounts.map(Max.function(RealScalar.ONE))));
+        visualSet.add(domain, values).setLabel(bulkParser.name());
       }
     }
-    XYDataset xyDataset = XYDatasets.create(map);
-    ListPlotBuilder listPlotBuilder = new ListPlotBuilder("lines", "files", "log lines", xyDataset);
-    JFreeChart jFreeChart = listPlotBuilder.getJFreeChart();
+    JFreeChart jFreeChart = ListPlot.of(visualSet);
     try {
       ChartUtils.saveChartAsPNG(new File(directory, "lines.png"), jFreeChart, 1024, 768);
     } catch (Exception exception) {
