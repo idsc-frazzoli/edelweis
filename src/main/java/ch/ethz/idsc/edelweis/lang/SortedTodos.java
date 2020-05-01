29e47512f5412bdd4c2adbe62fea5213c39a1958
clruch <claudio.ruch@gmail.com>
2020-05-01 21:43:38 +0200

    general functionality established, need to exclude exceptions ...

--- src/main/java/ch/ethz/idsc/edelweis/lang/SortedTodos.java
+++ src/main/java/ch/ethz/idsc/edelweis/lang/SortedTodos.java
@@ -7,13 +7,13 @@ import java.util.Random;
 
 /* package */ enum SortedTodos {
   ;
-  private static final Random random = new Random();
+  // private static final Random random = new Random();
   static final String UNKNOWN_IDENT = "unknown developer";
 
   public static Map<String, Map<String, String>> of(ParserText parserText) {
     Map<String, Map<String, String>> sortedTodos = new HashMap<>();
     parserText.todosPrint().forEach(todoLine -> {
-      String category = Integer.toString(random.nextInt(4));
+      String category = getDeveloper(todoLine);// Integer.toString(random.nextInt(4));
       // add map if not contained
       if (!sortedTodos.containsKey(category))
         sortedTodos.put(category, new LinkedHashMap<String, String>());
@@ -23,27 +23,18 @@ import java.util.Random;
   }
 
   static String getDeveloper(String todoLine) {
-    System.out.println("line in:" + todoLine);
     // no '@' --> no developer
     if (!todoLine.contains("@"))
       return UNKNOWN_IDENT;
-    // contains developer, extract, find position of identifier    
-    int identPos = todoLine.indexOf('@');
-
- 
-    
-    int endPos = identPos;
-    // advance to first non-space character
-    while (!todoLine.substring(endPos, endPos + 1).equals(" ") && endPos < todoLine.length()) {
-      System.out.println(todoLine.substring(endPos, endPos + 1));
-      ++endPos;
+    // contains developer, extract, find position of identifier
+    int identPos = todoLine.indexOf('@') + 1;
+    // remove any spaces before idetifier
+    while (todoLine.substring(identPos, identPos + 1).equals(" ") && identPos < todoLine.length()) {
+      ++identPos;
     }
-    // extract developer name
-    String subString = todoLine.substring(identPos + 1, endPos);
-    System.out.println(subString);
-    return subString;
-    // return todoLine;
+    // first word after identifier is developer
+    String restOfLine = todoLine.substring(identPos, todoLine.length());
+    String developer = restOfLine.split(" ")[0];
+    return developer;
   }
-
-
 }
--- src/test/java/ch/ethz/idsc/edelweis/lang/SortedTodosTest.java
+++ src/test/java/ch/ethz/idsc/edelweis/lang/SortedTodosTest.java
@@ -13,21 +13,14 @@ public class SortedTodosTest extends TestCase {
   public void testSimple() {
     List<String> todoLines = new ArrayList<>();
     todoLines.add("                        // TODO @Hans 20190901 remove soon if no errors");
+    todoLines.add("                        // TODO @ Hans 20190901 remove soon if no errors");
     todoLines.add("// TODO we did not name anybody here...errors");
-    todoLines.add("// TODO @ Fritz  resolve blabla ");
-    
-    List<String> developers = todoLines.stream().map(s->SortedTodos.getDeveloper(s)).collect(Collectors.toList());    
+    todoLines.add("// TODO @ Fritz resolve blabla ");
+    List<String> developers = todoLines.stream().map(s -> SortedTodos.getDeveloper(s)).collect(Collectors.toList());
     // --
-    
     Assert.assertTrue(developers.get(0).equals("Hans"));
-    Assert.assertTrue(developers.get(1).equals(UNKNOWN_IDENT));
-    
-    System.out.println("<"+developers.get(2)+">");
-
-    
-
-    
-//    String developer = SortedTodos.getDeveloper(todoLine);
-//    System.out.println("\n\ndeveloper: " + developer);
+    Assert.assertTrue(developers.get(1).equals("Hans"));
+    Assert.assertTrue(developers.get(2).equals(UNKNOWN_IDENT));
+    Assert.assertTrue(developers.get(3).equals("Fritz"));
   }
 }
